95d29af0e091315876ada27c2d101a67e88f81b9
Zalbezal <jeppe-christiansen@hotmail.com>
2023-11-18 18:29:46 +0100

    First description

--- main.py
+++ main.py
@@ -1,84 +1,10 @@
 # SoundboardBuilder
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-
-
-
-
-
-
-
-
-
-
+#   Build a TKInter interface
+#       Description of functionality
+#       Contains button for filepath selection, and a line for filepath
+#           Button does the following
+#               # For each folder in the filepath - Create headline for the soundbuttons
+#                   # for each file in the subfolder, create a soundboard button 
 
 
 print("Hello world 123")
